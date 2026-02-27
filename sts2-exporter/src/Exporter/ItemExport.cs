b998c8ef3555c064098470d26b94c226203f8676
ocean <ocean@ocean.lol>
2026-02-27 02:31:38 +0000

    tidy up random image paths for energy icons

--- sts2-exporter/src/Exporter/ItemExport.cs
+++ sts2-exporter/src/Exporter/ItemExport.cs
@@ -8,10 +8,16 @@ namespace STS2Export.Exporter;
 public abstract partial class ItemExport {
     public ModExport Mod;
 
+    [GeneratedRegex("\\[img.*?\\/img\\]")]
+    private static partial Regex BBCodeImgRegex();
+    private static readonly Regex BBCodeImgSubstitutor = BBCodeImgRegex();
     [GeneratedRegex("\\[.*?\\]")]
     private static partial Regex BBCodeRegex();
     private static readonly Regex BBCodeSubstitutor = BBCodeRegex();
-    protected static string StripBBCodeTags(string s) => Regex.Unescape(BBCodeSubstitutor.Replace(s, static m => ""));
+    protected static string StripBBCodeTags(string s) => Regex.Unescape(BBCodeSubstitutor.Replace(BBCodeImgSubstitutor.Replace(s, static m =>{
+        var path = m.Value["[img]".Length..(m.Value.Length-"[/img]".Length)];
+        return $"{{img={path[(path.LastIndexOf('/')+1)..path.LastIndexOf('.')]}}}";
+    }), static m => ""));
 }
 
 public interface IImageExport {   
