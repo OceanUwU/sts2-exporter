47d2aa14aa59dd8540db05fe888535541ea5dd10
ocean <ocean@ocean.lol>
2026-03-05 19:41:29 +0000

    update export script

--- export.sh
+++ export.sh
@@ -2,3 +2,7 @@
 cd "$(dirname "$0")"
 dotnet build
 megadot --headless --export-pack "Windows Desktop" ./STS2Export.pck
+rm -f STS2Export.zip
+wget https://raw.githubusercontent.com/scriban/scriban/refs/heads/master/license.txt -O Scriban-LICENSE.txt
+zip -rj STS2Export.zip .godot/mono/temp/bin/Debug/STS2Export.dll .godot/mono/temp/bin/Debug/Scriban.dll STS2Export.pck Scriban-LICENSE.txt
+rm -f Scriban-LICENSE.txt
