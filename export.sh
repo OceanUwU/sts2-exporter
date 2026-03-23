251e5ddb265984f837bc02ba595808aa467fe6e7
ocean <ocean@ocean.lol>
2026-03-23 20:51:31 +0000

    new mod manifest fields

--- export.sh
+++ export.sh
@@ -4,5 +4,6 @@ dotnet build
 megadot --headless --export-pack "Windows Desktop" ./STS2Export.pck
 rm -f STS2Export.zip
 wget https://raw.githubusercontent.com/scriban/scriban/refs/heads/master/license.txt -O Scriban-LICENSE.txt
-zip -rj STS2Export.zip .godot/mono/temp/bin/Debug/STS2Export.dll .godot/mono/temp/bin/Debug/Scriban.dll STS2Export.pck Scriban-LICENSE.txt
+zip -rj STS2Export.zip .godot/mono/temp/bin/Debug/STS2Export.dll .godot/mono/temp/bin/Debug/Scriban.dll STS2Export.pck Scriban-LICENSE.txt ./mod_manifest.json
+printf "@ mod_manifest.json\n@=STS2Export.json\n" | zipnote -w STS2Export.zip
 rm -f Scriban-LICENSE.txt
